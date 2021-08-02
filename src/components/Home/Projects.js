aae9ffe2831209c1697a58425faa0e96449b677a
Harshita Saxena <64150164+har882@users.noreply.github.com>
2021-08-03 00:23:07 +0530

    Revert "updated project"
    This reverts commit 9debd547c0899f43605cb6b75aa5c19037c1a588.

--- src/components/Home/Projects.js
+++ src/components/Home/Projects.js
@@ -31,10 +31,9 @@ const Projects = () => {
               <ProjectCard
                 
                 isBlog={false}
-                title="Clickstream Data Pipeline"
-                description="This group project is done under the Target Corporation. We designed and developed a Data 
-pipeline that delivers clean and good quality Clickstream Data using Spark and Scala."
-                link="https://github.com/har882/Clickstream-data-pipeline"
+                title="Gym Management System"
+                description="The system helps the administrator to manage the members, trainers of gym. It helps the new member
+                to decide the package they want to choose and provides payment facility."
                 
               />
             </Col>
